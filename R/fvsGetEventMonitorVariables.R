38b8d8f64de1f5090f6db53efe306b1ae0e80e1d
Nick Crookston <ncrookston.fs@gmail.com>
2012-06-04 00:34:35 +0000

    Modified EM interface so that NA's are returned when the EM variable is not available, coded up add trees function.
    git-svn-id: svn+ssh://svn.code.sf.net/p/open-fvs/code/rFVS@306 90b89b48-9230-0de4-5065-812dd12741b8

--- R/fvsGetEventMonitorVariables.R
+++ R/fvsGetEventMonitorVariables.R
@@ -6,8 +6,9 @@ function(vars)
   for (name in vars)
   {
     nch = as.integer(nchar(name))
-    all = c(all,.Fortran("fvsevmonattr",name,nch,"get",
-                as.double(0),as.integer(0))[[4]])
+    ans = .Fortran("fvsevmonattr",name,nch,"get",
+                as.double(0),as.integer(0))
+    all = c(all,if (ans[[5]] == 0) ans[[4]] else NA)
   }
   names(all) = vars
   all
