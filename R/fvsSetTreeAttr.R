70c8a1b52d5b64e4de61b0de47bc1c1a8bac1388
Nick Crookston <ncrookston.fs@gmail.com>
2012-11-10 01:15:56 +0000

    Flushed out code to set tree attribute values.
    git-svn-id: svn+ssh://svn.code.sf.net/p/open-fvs/code/rFVS@456 90b89b48-9230-0de4-5065-812dd12741b8

--- R/fvsSetTreeAttr.R
+++ R/fvsSetTreeAttr.R
@@ -1,6 +1,32 @@
-fvsSetTreeAttr <-
-function( ... )
-{ 
-# TODO: write function
-NULL }
+fvsSetTreeAttrs <-
+function(vars)
+{
+  ntrees = fvsGetDims()["ntrees"]
+  action = "set"
+  vars = as.list(vars)
+  rtn = 0
+  for (name in names(vars))
+  {
+    atr = as.numeric(vars[[name]])
+    if (length(atr) != ntrees) 
+    {
+      warning("Length of '",name,"' must be ",ntrees)
+      next
+    }
+    if (any(is.na(atr)))
+    {
+      warning ("NA found for variable '",name,"'")
+      next
+    }
+    nch =nchar(name)
+    ans = .Fortran("fvsTreeAttr",name,nch,action,ntrees,atr,as.integer(0))
+    if (ans[[6]] != 0) 
+    {
+      rtn = if (ans[[6]] > rtn) ans[[6]] else rtn
+      warning ("error assigning variable '",name,"'")
+      next
+    }
+  }
+  rtn
+}
 
