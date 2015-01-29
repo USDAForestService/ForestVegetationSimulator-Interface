6dac1880e0daeda0fc052f0b1e84a0c801b9c85e
Nick Crookston <ncrookston.fs@gmail.com>
2015-01-29 19:40:04 +0000

    Added fvsGetUnitConversion()
    git-svn-id: svn+ssh://svn.code.sf.net/p/open-fvs/code/rFVS@1408 90b89b48-9230-0de4-5065-812dd12741b8

---
+++ R/fvsGetUnitConversion.R
@@ -0,0 +1,8 @@
+fvsUnitConversion <-
+function(name)
+{
+  nch =nchar(name)
+  ans = .Fortran("fvsUnitConversion",name,nch,as.numeric(0),as.integer(0))
+  if (ans[[4]] == 0) return(ans[[3]]) else return(NA)
+}
+
