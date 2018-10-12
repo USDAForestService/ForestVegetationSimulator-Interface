6e94d9ac2a6ae22284a923b20d3fbea47b298eda
Mike Shettles <michael.a.shettles@usda.gov>
2018-10-12 17:43:38 +0000

    Removed check that searches for the libfvsSQL.dll file if on windows, since the interface is now designed to run on the non-ODBC q-family DLLs
    git-svn-id: svn+ssh://svn.code.sf.net/p/open-fvs/code/rFVS@2525 90b89b48-9230-0de4-5065-812dd12741b8

--- R/fvsLoad.R
+++ R/fvsLoad.R
@@ -19,14 +19,14 @@ function(fvsProgram,bin="../../trunk/bin")
       remove(".FVSLOADEDLIBRARY",envir=.GlobalEnv)
       lapply (loaded,dyn.unload)
     }
-    # if on windows, we also need the sql dll.
-    if (.Platform$OS.type == "windows")
-    {
-      sql = paste(bin,"libfvsSQL.dll",sep="")
-      load = dyn.load(sql)[[3]]
-      if (! load) stop (paste (sql,"was not loaded."))
-      loaded$sql = sql
-    }
+    # # if on windows, we also need the sql dll.
+    # if (.Platform$OS.type == "windows")
+    # {
+    #   sql = paste(bin,"libfvsSQL.dll",sep="")
+    #   load = dyn.load(sql)[[3]]
+    #   if (! load) stop (paste (sql,"was not loaded."))
+    #   loaded$sql = sql
+    # }
     load = dyn.load(file)[[3]]
     if (! load) stop (paste (file,"was not loaded."))
     loaded$pgm = file
