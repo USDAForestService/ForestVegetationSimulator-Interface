39c56d4ad62054b25daad0cfd95fa732e1aeaff7
Mike Shettles <michael.a.shettles@usda.gov>
2019-11-07 00:01:54 +0000

    Added a stipulation in the new KCP-handling ode that, if it’s a DBS-duplicate, it’s POTFIRE, and the number of parameters is greater than 1 (the number for the DBS POTFIRE), it’s actually an old ass FFE POTFIRE (dbflag <- 2)
    git-svn-id: svn+ssh://svn.code.sf.net/p/open-fvs/code/rFVS@2842 90b89b48-9230-0de4-5065-812dd12741b8

--- shinyFVSOnlineDev/fvsRunUtilities.R
+++ shinyFVSOnlineDev/fvsRunUtilities.R
@@ -606,7 +606,13 @@ cat("writeKeyFile, num stds=",length(stds),
               test <- na.omit(test)
               # if the keyword has more parameters than the other keyword with the same name, it's a DBS keyword
               if(length(test) > altmore[match(toupper(strsplit(kcpconts[j]," ")[[1]][1]),dbmore)]) {
-                dbflag <- 1
+                # if it's an old ass KCP that has more than 1 additional parameter for POTFIRE, it's actually FFE not DBS
+                if (toupper(strsplit(kcpconts[j]," ")[[1]][1])=="POTFIRE" && length(test) > 2) {
+                  dbflag <- 2
+                } else 
+                {
+                  dbflag <- 1
+                }
               }
               # if the keyword has less parameters than the other keyword with the same name, it's a non-DBS keyword
               if(length(test)<=altmore[match(toupper(strsplit(kcpconts[j]," ")[[1]][1]),dbmore)]) {
