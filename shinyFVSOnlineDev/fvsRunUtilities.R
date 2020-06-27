f00706d0bb8b007f3060bf4fa85f6f1677620fd4
Mike Shettles <michael.a.shettles@usda.gov>
2020-06-27 02:34:47 +0000

    Previous commit accidentally didn't include changes Nick recently made with the autoDeloTab functionality. Revved back to his commit, then manually added my changes so that mine and his are both integrated.
    git-svn-id: svn+ssh://svn.code.sf.net/p/open-fvs/code/rFVS@3152 90b89b48-9230-0de4-5065-812dd12741b8

--- shinyFVSOnlineDev/fvsRunUtilities.R
+++ shinyFVSOnlineDev/fvsRunUtilities.R
@@ -235,7 +235,7 @@ cat("writeKeyFile, num stds=",length(stds),
   if (length(stds)==0) return("No stands to process.")
   dbExecute(dbIcon,'drop table if exists temp.RunStds')                   
   dbWriteTable(dbIcon,DBI::SQL("temp.RunStds"),data.frame(RunStds = stds))
-
+ 
   # get the preferred ids depending on the table that was used to build the run
   intable=toupper(fvsRun$refreshDB)
   queryIDs=switch(intable,
@@ -398,9 +398,12 @@ cat ("processing std=",std$sid," sRows=",sRows," sRowp=",sRowp,"\n")
     # "checking" the FVS Outputs suppresses adding autoDelOTab so make that logical switch here
     autos = if (is.null(names(fvsRun$autoOut))) unlist(fvsRun$autoOut) else 
                 unlist(fvsRun$autoOut[["autoOut"]])
-    autos = if ("autoDelOTab" %in% autos) setdiff(autos,"autoDelOTab") else 
-                c(autos,"autoDelOTab")
-    for (out in autos) if (exists(out)) eval(parse(text=paste0("cat(",out,",file=fc)")))
+    autos = if ("autoDelOTab" %in% autos) 
+    { 
+      aa = setdiff(autos,"autoDelOTab")
+      unlist(lapply(aa,function(a) {aw = paste0(a,".withText"); if (exists(aw)) aw else a}))
+    } else c(autos,"autoDelOTab")
+    for (out in autos) if (exists(out) && !is.null(out)) eval(parse(text=paste0("cat(",out,",file=fc)")))
 
     if (!is.null(fvsRun$autoOut[["svsOut"]]) && !is.null(fvsRun$autoOut[["svsOut"]][["svs"]]) && 
       exists("autoSVS"))
@@ -2028,7 +2031,7 @@ addStandsToRun <- function (session,input,output,selType,globals,dbGlb)
     addkeys = list()
     if (!is.null(addfiles) && !is.na(addfiles))
     { 
-      fns=scan(text=addfiles,what="character",sep=",",quiet=TRUE)
+      fns=scan(text=addfiles,what="character",sep="\n",quiet=TRUE)
       semicolon <- grep(";",fns)
       if(length(semicolon)){
         splits <- unlist(strsplit(fns[semicolon],";"))
@@ -2174,7 +2177,7 @@ cat ("nreps=",nreps," rwts=",rwts," (recycled as needed)\n")
             addfiles <- AddFiles(globals$inData$
                         FVS_GroupAddFilesAndKeywords[grow,"ADDFILES"])
             for (addf in names(addfiles))
-              newgrp$cmps[[length(newgrp$cmps)+1]] <-
+                newgrp$cmps[[length(newgrp$cmps)+1]] <-
                 mkfvsCmp(kwds=as.character(addfiles[addf]),uuid=uuidgen(),atag="k",exten="base",
                       kwdName=paste0("AddFile: ",addf),
                         title=paste0("AddFile: ",addf))
