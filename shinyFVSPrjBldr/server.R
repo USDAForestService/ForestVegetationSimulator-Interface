cd0839d3fed601a96be7a906c0c1c5d2f4f5b507
Nick Crookston <ncrookston.fs@gmail.com>
2021-07-06 19:37:26 +0000

    shinyFVSPrjBldr: Modified to create new-style projects
    git-svn-id: svn+ssh://svn.code.sf.net/p/open-fvs/code/rFVS@3679 90b89b48-9230-0de4-5065-812dd12741b8

--- shinyFVSPrjBldr/server.R
+++ shinyFVSPrjBldr/server.R
@@ -7,53 +7,32 @@ shinyServer(function(input, output, session) {
   
   source("prjListEmail.R")
   source("uuidgen.R")
-  source("settings.R")
   trim <- function (x) gsub("^\\s+|\\s+$","",x)
-  avail = gsub("[.]so$","",dir (fvsBinDir,pattern="[.]so$"))
-  keep = intersect(avail,names(pgmList))
-  topick = as.list(keep)
-  names(topick) = unlist(pgmList[keep])
-  output$uivariants <- renderUI(
-    checkboxGroupInput("uivariants","Select FVS variants", 
-              choices=topick,selected="FVSie",inline=TRUE))
 # pop the sink stack
-while(sink.number()) sink()
-try(sink("FVSPrjBldr.log"))
-cat (date(),"\n")
-cat ("length(topick)=",length(topick),"\n")
+  while(sink.number()) sink()
+  try(sink("FVSPrjBldr.log"))
+  cat (date(),"\n")
 
   observe({
     if (input$submitnew == 0) return()
     isolate({
-      if (is.null(input$uivariants)) return()
       if (nchar(input$title)==0) return()
       emailnew = trim(input$emailnew)
       if (nchar(emailnew)<5 && regexpr("@",emailnew) < 2) return()
       uuid = uuidgen()
-      workDir = paste0(fvsWork,uuid)
-cat("workDir=",workDir,"\n")
-      dir.create(workDir)
-      binDir = paste0(workDir,"/FVSbin/")
-      # fvs libraries
-      dir.create(binDir)
-      cmd = paste0("cp ",paste(paste0(fvsBinDir,
-            paste0(input$uivariants,".so"),collapse=" ")," ",binDir))
-cat ("fvsbin cmd=",cmd,"\n")
-      system (cmd)
-      # shiny code, etc
-      needed=paste(paste0(fvsOnlineDir,FVSOnlineNeeded),collapse=" ")
-      system (paste0("cp -R ",needed," ",workDir))
-      # rFVS
-      dir.create(tr <- paste0(workDir,"/rFVS"))
-      system (paste0("cp -R ",rFVSDir," ",tr))
-      
+      workDir = paste0("/home/shiny/FVSwork/",uuid)
+     cat("workDir=",workDir,"\n")
+     dir.create(workDir)
+     setwd(workDir)
+     cat ('library(fvsOL)\nfvsOL(fvsBin="../../FVS/bin")\n',file="app.R")
+
       # projectId file...
 cat("email=",emailnew,"\ntitle=",input$title,"\n")
       cat(file=paste0(workDir,"/projectId.txt"),
           "email=",emailnew,"\ntitle=",input$title,"\n")
       rptFile = tempfile()
       con = file(rptFile,"w")
-      link = paste0("http://forest.moscowfsl.wsu.edu/FVSwork/",uuid)
+      link = paste0("https://charcoal2.cnre.vt.edu/FVSwork/",uuid)
       cat (file=con,"Here is a link to the project named:",input$title,"\n\n")
       cat (file=con,link,"\n\n")
       cat (file=con,"Note that this project may be removed",
@@ -63,7 +42,7 @@ cat("email=",emailnew,"\ntitle=",input$title,"\n")
       mailCmd = paste('mailx -a "From: FVSOnline"',
        '-a "Subject: New project: ',input$title,'"',
        '-a "Reply-To: Nicholas Crookston <ncrookston.fs@gmail.com>"',
-       '-a "Cc: ncrookston.fs@gmail.com" --',emailnew,'<',rptFile)      
+       '-a  --',emailnew,'<',rptFile)      
 cat("mailCmd=",mailCmd,"\n")
       system (mailCmd)
       if (nchar(input$title)) 
--- shinyFVSPrjBldr/ui.R
+++ shinyFVSPrjBldr/ui.R
@@ -8,20 +8,21 @@ shinyUI(fluidPage(
     ".form-group {margin-bottom:6px}"))),
   verticalLayout(
     h2("Forest Vegetation Simulator (FVS-Online)"),
-    p("Set up an FVSOnline project by filling out this form, including the ",
-      "selection the FVS variants you might use. You will be sent an Email ",
+    h3("Hosted by Virginia Tech, College of Natural Resources and Environment",
+      " / Department of Forest Resources and Environmental Conservation."),
+    h5("Set up an FVSOnline project by filling out this form. ",
+      "You will be sent an Email ",
       "with a link to this project.  Note that your project(s) will be ",
       "removed from this server 60 days after the last access."),
     p("Check your Email spam files if you don't get the Email promptly."),
     tags$style(type="text/css", "#title { width: 500px; }"),
     textInput("title", "Your new project title"),
     textInput("emailnew", "Your Email address"),
-    uiOutput("uivariants"),    
     actionButton("submitnew","Submit"),
     tags$style(type="text/css","#actionMsg{color:darkred;}"), 
     textOutput("actionMsg"),p(),
     div(style="height:1px;width:100%;background-color:lightgray;"),
-    p("Get a list of existing projects"),
+    h5("Get a list of existing projects"),
     textInput("emailexist", "Your Email address", ""),
     actionButton("submitexist","Submit"),p(),
     div(style="height:1px;width:100%;background-color:lightgray;"),
@@ -32,7 +33,14 @@ shinyUI(fluidPage(
     "authors or copyright holders be liable for any claim, damages or other ",
     "liability, whether in an action of contract, tort or otherwise, arising ",
     "from, out of or in connection with the software or the use or other dealings ",
-    "in the software.")
+    "in the software."),
+    p("This site is hosted by Virginia Tech, College of Natural Resources and Environment",
+      " / Department of Forest Resources and Environmental Conservation ",
+      "and is intended to be used for educational purposes. In no event will Virginia Tech ",
+      "be liable for any claim, damages or other ",
+      "liability, whether in an action of contract, tort or otherwise, arising ",
+      "from, out of or in connection with the software or the use or other dealings ",
+      "in the software.")
   )
 ))
 
