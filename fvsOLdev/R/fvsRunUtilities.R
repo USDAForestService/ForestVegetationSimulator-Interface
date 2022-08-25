4eb11d6b9f0a367fcfa188bb7250afab428b820d
Michael Shettles <michael.a.shettles@usda.gov>
2022-08-25 17:36:50 -0600

    fvsOLdev: 1. Added AGECMP to the list of EM variables, and INDEX() and POINTID() to the list of EM functions, in the freeform editor dropdowns. The prior two automatically get inserted to the wherever the cursor is, and the latter (POINTID()) has a small window created for it with a single text box as it’s parameter. 2. Bug fix: deleting the very last stand replicate from a run when there are more than 50 stands replicates in the run contents caused a crash and corrupted the run, preventing a relaunch of the interface.

--- fvsOLdev/R/fvsRunUtilities.R
+++ fvsOLdev/R/fvsRunUtilities.R
@@ -1,4 +1,4 @@
-# $Id$
+# $Id: fvsRunUtilities.R 3982 2022-05-10 18:07:19Z mshettles521 $
 
 loadStandTableData <- function (globals, dbIcon)
 {
@@ -231,6 +231,7 @@ cat("mkSimCnts, foundStand=",foundStand," start=",start," end=",end,
   paste0("length(list)=",length(list)) else sels,"\n")
     if (length(fvsRun$stands)) for (i in start:end) 
     {
+      if(length(fvsRun$stands) < i) break
       ## these two lines are needed to deal with old runs that may not have these elements in the stand class
       if (class(fvsRun$stands[[i]]$rep  )!="numeric") fvsRun$stands[[i]]$rep  =0
       if (class(fvsRun$stands[[i]]$repwt)!="numeric") fvsRun$stands[[i]]$repwt=1
