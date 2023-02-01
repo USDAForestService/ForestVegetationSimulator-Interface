3c202662c3c1119e626a63e24cb78bbc608c4d00
Nicholas Crookston <ncrookston.fs@gmail.com>
2023-02-01 08:38:43 -0800

    rFVS: Improved the documentation for fvs[Get|Set]SpeciesAttrs, reset the revision date in DESCRIPRTION

--- rFVS/R/fvsGetSpeciesAttrs.R
+++ rFVS/R/fvsGetSpeciesAttrs.R
@@ -1,10 +1,28 @@
 #' Return the values of species-specific data
 #'
-#' @param vars a character vector of species-level attributes desired:
+#' @param vars a character vector of species-level attributes desired. See 
+#'   \url{https://www.fs.usda.gov/fmsc/ftp/fvs/docs/gtr/EssentialFVS.pdf} for related details.
+#'   The attributes can be any of the following:
 #' \tabular{cl}{
-#'    spccf	    \tab CCF for each species, recomputed in FVS so setting will likely have no effect\cr
+#'    spccf	    \tab CCF for each species, recomputed in FVS so setting may have no effect (depending on variant)\cr
 #'    spsdi	    \tab SDI maximums for each species \cr
-#'    spsiteindx \tab Species site indices\cr}
+#'    spsiteindx \tab Species site indices\cr
+#'    bfmind     \tab board foot minimum dbh for each species \cr
+#'    bftopd     \tab board foot top diameter for each species \cr
+#'    bfstmp     \tab board foot stump height for each species \cr
+#'    frmcls     \tab board foot form class for each species \cr
+#'    bfmeth     \tab board foot calculation method for each species \cr
+#'    mcmind     \tab murchantable cubic volume minimum dbh for each species \cr
+#'    mctopd     \tab murchantable cubic volume top diameter for each species \cr
+#'    mcstmp     \tab murchantable cubic volume stump height for each species \cr
+#'    mcmeth     \tab murchantable cubic volume calculation method for each species \cr
+#'    baimult    \tab basal area increment multiplier for each species \cr
+#'    htgmult    \tab height growth multiplier for each species \cr
+#'    mortmult   \tab mortality rate multiplier for each species \cr
+#'    mortdia1   \tab lower diameter limit to apply the multiplier for each species \cr
+#'    mortdia2   \tab upper diameter limit to apply the multiplier for each species \cr
+#'    regdmult   \tab multiplier for diameter growth of regeneration for each species \cr
+#'    reghmult   \tab multiplier for height growth of regeneration for each species}
 #' @return a data.frame where the columns are attributes and the rows are species. 
 #' @examples
 #'    #edit fvsLoad to reflect where FVSbin is stored on your system. 
--- rFVS/R/fvsSetSpeciesAttr.R
+++ rFVS/R/fvsSetSpeciesAttr.R
@@ -1,11 +1,29 @@
 #' Set the values of species-specific data
 #'
 #' @param vars a named list of numeric vectors where the name corresponds to an attribute and
-#'  the vector contains values for each species (in order). The attributes can be any of these:
-#'  \tabular{cl}{
-#'    spccf	    \tab CCF for each species, recomputed in FVS so setting will likely have no effect\cr
+#'   the vector contains values for each species (in order). See 
+#'   \url{https://www.fs.usda.gov/fmsc/ftp/fvs/docs/gtr/EssentialFVS.pdf} for related details.
+#'   The attrubytes can be any of the following:
+#' \tabular{cl}{
+#'    spccf	    \tab CCF for each species, recomputed in FVS so setting may have no effect (depending on variant)\cr
 #'    spsdi	    \tab SDI maximums for each species \cr
-#'    spsiteindx \tab Species site indices\cr}
+#'    spsiteindx \tab Species site indices\cr
+#'    bfmind     \tab board foot minimum dbh for each species \cr
+#'    bftopd    \tab board foot top diameter for each species \cr
+#'    bfstmp     \tab board foot stump height for each species \cr
+#'    frmcls     \tab board foot form class for each species \cr
+#'    bfmeth     \tab board foot calculation method for each species \cr
+#'    mcmind     \tab murchantable cubic volume minimum dbh for each species \cr
+#'    mctopd     \tab murchantable cubic volume top diameter for each species \cr
+#'    mcstmp     \tab murchantable cubic volume stump height for each species \cr
+#'    mcmeth     \tab murchantable cubic volume calculation method for each species \cr
+#'    baimult    \tab basal area increment multiplier for each species \cr
+#'    htgmult    \tab height growth multiplier for each species \cr
+#'    mortmult   \tab mortality rate multiplier for each species \cr
+#'    mortdia1   \tab lower diameter limit to apply the multiplier for each species \cr
+#'    mortdia2   \tab upper diameter limit to apply the multiplier for each species \cr
+#'    regdmult   \tab multiplier for diameter growth of regeneration for each species \cr
+#'    reghmult   \tab multiplier for height growth of regeneration for each species}
 #' @return scalar integer 0 signals OK and 1 signals an error.
 #' @examples
 #'    #edit fvsLoad to reflect where FVSbin is stored on your system. 
